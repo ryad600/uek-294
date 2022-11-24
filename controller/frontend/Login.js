495f67cadf1d18b2ec9d78d553de57b76d89a914
Ryad Mohamed <ryad.2002@hotmail.com>
2022-11-24 03:27:25 +0100

    login almost finished.

--- controller/frontend/Login.js
+++ controller/frontend/Login.js
@@ -1,33 +1,33 @@
-var request
 
-function onRequestUpdate(event) {
-    if (exampleRequest.readyState < 4) {
-        return;
-    }
-    
-}
+var request;
 
 function onLoginButtonPressed(event) {
     var data = {
-        name: loginName,
-        password: loginPassword
+        username: loginName.value,
+        password: loginPassword.value
     };
     request = new XMLHttpRequest();
-    request.open("POST", "../API/V1/public/controller/categories/get_all_categories.php");
+    request.open("POST", "../API/V1/Login");
     request.onreadystatechange = onRequestUpdate;
     request.send(JSON.stringify(data));
 }
 
-
-
+//hello
 function onRequestUpdate(event) {
-    if (exampleRequest.readyState < 4) {
+    if (request.readyState < 4) {
         return;
     }
-    var responseData = JSON.parse(exampleRequest.responseText);
+    var responseData = request.responseText;
+    if (responseData == true) {
+        alert("Login successful.")
+    }
+    else {
+        alert("Login failed.")
+    }
 }
-
 
 var loginButton = document.getElementById("login-button");
 var loginName = document.getElementById("login-name");
 var loginPassword = document.getElementById("login-password");
+
+loginButton.addEventListener("click", onLoginButtonPressed);
